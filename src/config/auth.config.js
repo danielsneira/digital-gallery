4842e46e073dfab2878d685b7acadf0511f74069
danielsneira <danos.neira@gmail.com>
2021-12-07 17:03:46 -0500

    art routes

--- src/config/auth.config.js
+++
@@ -1,3 +0,0 @@
-const secret = "secret-key" // temp key, change to .env variable in production
-
-export default secret
--- src/controllers/art.js
+++ src/controllers/art.js
@@ -1,11 +1,67 @@
-import Art from '../models/art.js'
+import Art from "../models/art.js";
 
 const artControllers = {
-  artGet: async (req, res) => {
-    res.json({
-      "message": "todo good"
-    })
-  }
-}
+  getArt: async (req, res) => {
+    const art = await Art.find().populate("artist", "fullname").populate({
+      path: 'comments',
+      populate: {
+        path: "artist",
+        model: "Artist"
+      } 
+    }).sort('-createdAt');
 
-export default artControllers
+    res.json(art);
+  },
+
+  getArtByArtist: async (req, res) => {
+    const { id } = req.params;
+    const art = await Art.find({artist: id}).populate("artist", "fullname")
+
+    res.json(art);
+  },
+
+  artPost: async (req, res) => {
+    const { artist, caption, image } = req.body;
+
+    const art = new Art({ artist, caption, image });
+    await art.save();
+
+    res.json({
+      art,
+    });
+  },
+
+  artComment: async (req, res) => {
+    const { artId, artist, comment } = req.body;
+
+    const newComment = { artist: artist, comment: comment };
+    // add comment to art
+    const art = await Art.findByIdAndUpdate(artId, { $push: {comments: newComment} });
+    
+    await art.save();
+
+    res.json({
+      art,
+    });
+  },
+
+  artToggleLike: async (req, res) => {
+    const { artId, artist, like } = req.body;
+    let art;
+
+    if (like){
+    // add like to art
+    art = await Art.findByIdAndUpdate(artId, { $push: {likes: artist} });
+    
+  } else {
+      art = await Art.findByIdAndUpdate(artId, { $pull: {likes: artist} });
+    }
+    await art.save();
+
+    res.json({
+      art,
+    });
+  },
+};
+
+export default artControllers;
